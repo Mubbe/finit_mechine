ca9ae8b65b60243b8465d8c9396916e995b452e2
Mubbe <88598179+Mubbe@users.noreply.github.com>
2026-02-27 08:03:55 +0100

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,82 @@
+using Godot;
+using System;
+using System.Net.NetworkInformation;
+
+public partial class Enemy : Node3D
+{
+    [Export] public CharacterBody3D player;
+    
+
+
+
+    // Called when the node enters the scene tree for the first time.
+    enum Npcstate
+    {
+        idle,
+        chasing
+    }
+
+    public override void _Ready()
+    {
+        
+        
+            var area = GetNode<Area3D>("Area3D");
+            area.BodyEntered += BodyEntered;
+            area.BodyExited += BodyExited;
+        
+    }
+
+
+    private Npcstate currentstate = Npcstate.idle;
+    public override void _Process(double delta)
+    {
+        
+        switch (currentstate)
+        {
+            case Npcstate.idle:
+                {
+
+                    
+                    break;
+                    
+                }
+            case Npcstate.chasing:
+                {
+                    // Move towards the player but keep the same Y position:
+                    Vector3 targetPos = player.GlobalPosition;
+                    targetPos.Y = GlobalPosition.Y;
+
+
+                    Vector3 dir = (targetPos - GlobalPosition).Normalized();
+                    float speed = 2f; 
+                    GlobalTranslate(dir * speed * (float)delta);
+                    GD.Print("Chasing player...");
+                    
+                    break;
+                }
+
+
+        }
+    }
+
+    void BodyEntered(Node3D body)
+    {
+        GD.Print("BodyEntered called with: ", body.Name);
+        if (body == player)
+        {
+            currentstate = Npcstate.chasing;
+            
+        }
+    }
+
+    void BodyExited(Node3D body)
+    {
+        if (body == player)
+        {
+            GD.Print("Player exited");
+            currentstate = Npcstate.idle;
+        }
+    }
+
+
+}
